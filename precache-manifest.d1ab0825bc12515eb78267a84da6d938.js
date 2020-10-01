a73ae8467fa3b4bc3da658c1e2485f74eb3b9031
Chidozie <chidough14@gmail.com>
2020-10-01 19:51:03 +0200

    Updates

---
+++ precache-manifest.d1ab0825bc12515eb78267a84da6d938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0a1e572bdf90e2983ddfab5a05b2c8",
+    "url": "/simumatik-scoreboard/index.html"
+  },
+  {
+    "revision": "f6cb457b4da2afd95a23",
+    "url": "/simumatik-scoreboard/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "e9246bef88185b1e9c06",
+    "url": "/simumatik-scoreboard/static/css/main.8a63f927.chunk.css"
+  },
+  {
+    "revision": "f6cb457b4da2afd95a23",
+    "url": "/simumatik-scoreboard/static/js/2.9b0c8267.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/simumatik-scoreboard/static/js/2.9b0c8267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9246bef88185b1e9c06",
+    "url": "/simumatik-scoreboard/static/js/main.c509cc62.chunk.js"
+  },
+  {
+    "revision": "000b160544c13a680612",
+    "url": "/simumatik-scoreboard/static/js/runtime-main.af3d2635.js"
+  }
+]);
